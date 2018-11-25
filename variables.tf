edad9ba9d4bac557042aa19d5b16dbcd7e021ae6
Florian Woerner <florian.woerner@onmyown.io>
2018-11-25 14:27:14 +0100

    Fix typos
    Signed-off-by: Florian Woerner <florian.woerner@onmyown.io>

--- variables.tf
+++ variables.tf
@@ -49,17 +49,17 @@ variable "daily_maintenance_window_start_time" {
 ############## Addon vars ###########
 # Should the following addons be disabled
 variable "http_load_balancing" {
-  description = "Should http_load_balancing addon be disableda? (true/false)"
+  description = "Should http_load_balancing addon be disabled? (true/false)"
 }
 
 variable "horizontal_pod_autoscaling" {
-  description = "Should horizontal_pod_autoscaling addon be disableda? (true/false)"
+  description = "Should horizontal_pod_autoscaling addon be disabled? (true/false)"
 }
 
 variable "kubernetes_dashboard" {
-  description = "Should kubernetes_dashboard addon be disableda? (true/false)"
+  description = "Should kubernetes_dashboard addon be disabled? (true/false)"
 }
 
 variable "network_policy_config" {
-  description = "Should network_policy_config addon be disableda? (true/false)"
+  description = "Should network_policy_config addon be disabled? (true/false)"
 }
